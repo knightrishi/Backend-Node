d9de687a4f52bf03b514deae16ebd3c9811d5b5f
knightrishi <arnav.ara05@gmail.com>
2025-09-20 19:26:18 +0530

    Url Parser done

--- Servers/index.js
+++ Servers/index.js
@@ -1,10 +1,16 @@
-const { Console } = require("console");
-const http=require("http")
-const fs=require("fs")
-const myServer=http.createServer((req,res)=>{
-const log=`${Date.now()} :${req.url} New Req Recieved.\n`;
-fs.appendFile('log.txt',log, (err,data)=>{//Always use Non-blocking function
-switch (req.url) {
+const http = require("http"); //http does parse req.url and does not diff between path and query paramete
+const fs = require("fs");
+const url = require("url"); //it is use for parsing the URL
+
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()} :${req.url} New Req Recieved.\n`;
+  const myUrl = url.parse(req.url);
+  console.log(myUrl);
+
+  if (req.url === "/favicon.ico") return res.end();
+  fs.appendFile("log1.txt", log, (err, data) => {
+    //Always use non blocking function
+    switch (req.url) {
       case "/":
         res.end("This is the Home Page");
         break;
@@ -16,11 +22,10 @@ switch (req.url) {
       default:
         res.statusCode = 404;
         res.end("Error 404 - File not found.");
-}
-})
-
+    }
+  });
 });
 
-myServer.listen(7070,()=>{
-    console.log("Server Chalu start hogaya")
+myServer.listen(7070, () => {
+  console.log("Server Chalu start hogaya");
 });
