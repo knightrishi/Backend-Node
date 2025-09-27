d10126d72064bb39c1ed383f7d909c047f23f069
knightrishi <arnav.ara05@gmail.com>
2025-09-27 20:44:46 +0530

    Controller File

---
+++ Short-URL/controllers/url.js
@@ -0,0 +1,19 @@
+const { nanoid }=require('nanoid')
+const URL=require('../models/url')
+
+async function handleGenerateNewURL(req,res) {
+    const shortID=nanoid(8);
+    const body=req.body;
+    if(!body.url) return res.status(400).json({ error: 'url is required'})
+    await URL.create({
+        shortId:shortID,
+        redirectURL:body.url,
+        visitHistory:[],
+    });
+
+    return res.json({ id: shortID})
+}
+
+module.exports={
+    handleGenerateNewURL,
+}
