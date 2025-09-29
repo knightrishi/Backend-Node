ddd76514f5fd8a40cf9ccad734fe4e611afa209f
knightrishi <arnav.ara05@gmail.com>
2025-09-29 20:41:22 +0530

    Login Function controller

---
+++ Short-URL/controllers/user.js
@@ -0,0 +1,39 @@
+const User=require('../models/user')
+const { v4:uuidv4 }=require('uuid')
+
+const { setUser }=require('../services/auth')
+
+async function handleUserSignUps(req,res) {
+    const {name, email, password}=req.body;
+    await User.create({
+        name,
+        email,
+        password,
+    });
+
+    //Jaise hi submit  buttyon of signup page pe click karuga to yaha pe redirect kar dega
+    return res.render("home");
+}
+
+async function handleUserLogin(req,res) {
+    const { email, password}=req.body;
+    const user=await User.findOne({email, password});
+
+    if(!user) return res.render("login",{
+        err:'Invalid Username or Password'
+    })
+
+    const sessionID=uuidv4();
+    setUser(sessionID,user);
+
+    res.cookie("uid", sessionID);
+
+
+    //Jaise hi submit  buttyon of signup page pe click karuga to yaha pe redirect kar dega
+    return res.redirect("/");
+}
+
+module.exports={
+    handleUserSignUps,
+    handleUserLogin,
+}
