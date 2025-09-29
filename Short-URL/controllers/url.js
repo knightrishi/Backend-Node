3a862a79ee9c7567ca947ed9b95ba96ad453da36
knightrishi <arnav.ara05@gmail.com>
2025-09-29 20:41:00 +0530

    Modifications

--- Short-URL/controllers/url.js
+++ Short-URL/controllers/url.js
@@ -1,21 +1,29 @@
-const { nanoid }=require('nanoid')
-const URL=require('../models/url')
+const { nanoid } = require('nanoid');
+const URL = require('../models/url');
 
-async function handleGenerateNewURL(req,res) {
-    const shortID=nanoid(8);
-    const body=req.body;
-    if(!body.url) return res.status(400).json({ error: 'url is required'})
+async function handleGenerateNewURL(req, res) {
+    const shortID = nanoid(8);
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: 'url is required' });
+
+    let userUrl = body.url;
+    
+    // This is the critical part that adds the protocol if it's missing
+    if (!userUrl.startsWith('http://') && !userUrl.startsWith('https://')) {
+        userUrl = `https://${userUrl}`;
+    }
+
     await URL.create({
-        shortId:shortID,
-        redirectURL:body.url,
-        visitHistory:[],
+        shortId: shortID,
+        redirectURL: userUrl, // Save the corrected, full URL
+        visitHistory: [],
     });
 
     return res.render("home", {
-        id:shortID,
-    })
+        id: shortID,
+    });
 }
 
-module.exports={
+module.exports = {
     handleGenerateNewURL,
-}
+};
